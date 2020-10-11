665c9bb6f748d6a9cacae4e0bbd635ba8fb1fd89
ismailAbdirahman <ismail960i960@gmail.com>
2020-10-11 20:41:09 +0530

    fixed a bug

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'Wrapper.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<UserModel>.value(
